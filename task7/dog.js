e97ac28e62d15704aa9d84536900172987b43635
unknown <abhijeetranawat13@gmail.com>
2020-08-06 03:02:17 +0530

    Task 7

---
+++ task7/dog.js
@@ -0,0 +1,13 @@
+//users current age
+const myage=21;
+//
+let earlyYears=2;
+earlyYears=earlyYears*10.5;
+let laterYears=myage-2;
+laterYears=laterYears*4;
+
+let myAgeInDogYears=earlyYears+laterYears;
+
+let myname="Abhijeet".toLowerCase();
+
+console.log(`My name is ${myname}. I am ${myage} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
